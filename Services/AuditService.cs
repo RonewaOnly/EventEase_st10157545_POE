e724651e4f72c992b578f237bdff037bf5dca9a9
Latitude E6530 <Latitude E6530@DESKTOP-3IVVUO6>
2026-03-15 14:58:25 -0700

    included the code for both Audit and Booking conflict code

--- Services/AuditService.cs
+++ Services/AuditService.cs
@@ -1,6 +1,28 @@
-﻿namespace EventEase_st10157545_POE.Services
+﻿using EventEase_st10157545_POE.Data;
+using EventEase_st10157545_POE.Models;
+namespace EventEase_st10157545_POE.Services
 {
     public class AuditService
     {
+        private readonly EventEaseDbContext _context;
+
+        public AuditService(EventEaseDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task LogAsync(int specialistId, string action, string table, int recordId, string? details = null)
+        {
+            _context.AuditLog.Add(new AuditLogViewModel { 
+                SpecialistID = specialistId,
+                Action = action,
+                TablesAffected = table,
+                RecordID = recordId,
+                Timestamp = DateTime.UtcNow,
+                Details = details
+            });
+
+            await _context.SaveChangesAsync();
+        }
     }
 }
--- Services/BookingConflictService.cs
+++ Services/BookingConflictService.cs
@@ -1,6 +1,71 @@
-﻿namespace EventEase_st10157545_POE.Services
+﻿using EventEase_st10157545_POE.Data;
+using EventEase_st10157545_POE.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventEase_st10157545_POE.Services
 {
     public class BookingConflictService
     {
+        private readonly EventEaseDbContext _context;
+
+        public BookingConflictService(EventEaseDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> HasConflictAsync(
+            int venueid,
+            DateTime eventDate,
+            TimeSpan startTime,
+            TimeSpan endTime,
+            int? excludeBookingId = null)
+        {
+            var query = _context.Bookings
+                .Where(b =>
+                    b.VenueID == venueid &&
+                    b.EventDate.Date == eventDate.Date &&
+                    b.Status != "Cancelled" &&
+                    b.StartTime < endTime &&
+                    b.EndTime > startTime
+                );
+
+            if(excludeBookingId.HasValue ) 
+                query = query.Where(b => b.BookingID != excludeBookingId.Value);
+
+            return await query.AnyAsync();
+        }
+
+        public async Task<List<Booking>> GetConflictAsync(
+            int venueId,
+            DateTime eventDate,
+            TimeSpan startTime,
+            TimeSpan endTime,
+            int? excludeBookingId = null
+            )
+        {
+            var query = _context.Bookings
+                .Include(b => b.Customer)
+                .Include(b => b.Event)
+                .Where(b =>
+                    b.VenueID == venueId &&
+                    b.EventDate.Date == eventDate.Date &&
+                    b.Status != "Cancelled" &&
+                    b.StartTime < endTime &&
+                    b.EndTime > startTime
+                );
+
+            if (excludeBookingId.HasValue)
+                query = query.Where(b => b.BookingID != excludeBookingId.Value);
+
+            return await  query.ToListAsync();
+        }
+
+        public async Task<bool> ExceedsCapacityAsync(int venueId, int guestCount)
+        {
+            var venue = await _context.Venue.FindAsync(venueId);
+            return venue != null && guestCount > venue.Capacity;
+        }
+
+
     }
 }
