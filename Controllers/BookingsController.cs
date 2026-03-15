3f84158ebca6318996e7aae6e0f61b3e842b9c20
Latitude E6530 <Latitude E6530@DESKTOP-3IVVUO6>
2026-03-15 06:31:53 -0700

    This is for creation of the controller files

---
+++ Controllers/BookingsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventEase_st10157545_POE.Controllers
+{
+    public class BookingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/EventsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventEase_st10157545_POE.Controllers
+{
+    public class EventsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
