369439bb8a8cf354483e5e470a94a67812c11a3e
Student-Ronewa <st10157545@rcconnect.edu.za>
2026-06-09 21:46:27 +0200

    migration

---
+++ Migrations/20260609182922_Part3_EventTypeAndVenueAvailability_new_addition.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventEase_st10157545_POE.Migrations
+{
+    /// <inheritdoc />
+    public partial class Part3_EventTypeAndVenueAvailability_new_addition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAvailable",
+                table: "Venue",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAvailable",
+                table: "Venue");
+        }
+    }
+}
